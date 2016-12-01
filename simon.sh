bd5da7cc376bdb67930799bc7c539de7dd7221b8
Frank Karlitschek <karlitschek@users.noreply.github.com>
2016-12-01 14:30:41 +0100

    Update simon.sh

--- simon.sh
+++ simon.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Some people work with a lot of terminal windows. 
+# Usually they approach a state where they have a terminal open for every directory that exists.
+# This scripts automates this and helps to reach an optimal working environment after just a few seconds.
+
 recurse() {
  for i in "$1"/*;do
     if [ -d "$i" ];then
